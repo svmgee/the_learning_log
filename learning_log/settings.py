858dc20def1fc21cf3f47b9df2a87ff2db060658
svmgee <samgarciarojas@gmail.com>
2025-01-22 10:55:44 -0500

    Edited some typos in settings.py. Changed STATICFILES_DIRS, removed 'learning_log'

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -159,7 +159,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'learning_log', 'static')
+        os.path.join(BASE_DIR, 'static')
     ]
     
     
